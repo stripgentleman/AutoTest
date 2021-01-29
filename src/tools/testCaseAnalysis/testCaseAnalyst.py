2d3d21181fc7831a7de73fdca3e677ace0374682
huangwenda <417198820@qq.com>
2021-01-29 18:34:05 +0800

    完善用例文件解析module的动态加载设计（70%）

--- src/tools/testCaseAnalysis/testCaseAnalyst.py
+++ src/tools/testCaseAnalysis/testCaseAnalyst.py
@@ -1,5 +1,7 @@
 import os
 import re
+import inspect
+from importlib import import_module
 
 from src.config import testCaseConfig
 
@@ -11,13 +13,14 @@ class TestCaseAnalyst:
 
     def __init__(self):
         self.type_analyst = {
-            '.xmind': 'src.tools.testCaseAnalysis.xmind.xmindAnalyst',
+            # .后缀名:解析类的包路径.类名.方法类
+            '.xmind': 'src.tools.testCaseAnalysis.xmind.xmindAnalyst.XmindAnalyst.analysis',
             '.excel': '',
         }
+        self.file_dict = self.get_case_name_dict()
 
     def get_case_name_dict(self):
         file_dict = dict()
-
         for case_path in self.case_path_list:
             for postfix in self.postfix_list:
                 pattern = re.compile(r'(.*)(' + postfix + ')$')
@@ -29,9 +32,20 @@ class TestCaseAnalyst:
                         file_dict[postfix].append(file_name)
         return file_dict
 
-    # def
+    def analysis(self):
+        for file_postfix in self.file_dict:
+            if len(self.file_dict[file_postfix]) == 0:
+                continue
+            module_path = self.type_analyst[file_postfix].split('.')
+            module_name = '.'.join(module_path[:-2])
+            analyst_module = import_module(module_name)
+            analyst_object = getattr(analyst_module, module_path[-2])
+            a = analyst_object()
+            analyst_method = getattr(a, module_path[-1])
+            analyst_method()
 
 
 if __name__ == '__main__':
     a = TestCaseAnalyst()
     print(a.get_case_name_dict())
+    a.analysis()
--- src/tools/testCaseAnalysis/xmind/xmindAnalyst.py
+++ src/tools/testCaseAnalysis/xmind/xmindAnalyst.py
@@ -1,8 +1,16 @@
 import zipfile
 import os
 
+
 class XmindAnalyst:
 
     @staticmethod
     def unpack_by_zipfile(file_name, out_put=None):
         return None
+
+    @staticmethod
+    def import_test():
+        print('import success')
+
+    def analysis(self):
+        print('temp')
