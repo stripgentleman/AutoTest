ae483c83f130946a7273a4df610018a7e4eaf256
stripgentleman <417198820@qq.com>
2021-02-01 20:42:10 +0800

    完善用例解析器代码（80%）

---
+++ src/tools/testCaseAnalysis/abstractAnalyst.py
@@ -0,0 +1,6 @@
+# 测试用例解析器的基础类，包含解析器的基本方法，如果要自定义解析器的话建议继承该类
+
+
+class AbstractAnalyst:
+
+    def json_result2method_lists(self, ):
--- src/tools/testCaseAnalysis/testCaseAnalyst.py
+++ src/tools/testCaseAnalysis/testCaseAnalyst.py
@@ -13,36 +13,45 @@ class TestCaseAnalyst:
 
     def __init__(self):
         self.type_analyst = {
-            # .后缀名:解析类的包路径.类名.方法类
+            # .后缀名:解析类的包路径.类名.方法名-----------------默认情况下该方法需有且仅有case_name参数
             '.xmind': 'src.tools.testCaseAnalysis.xmind.xmindAnalyst.XmindAnalyst.analysis',
             '.excel': '',
         }
-        self.file_dict = self.get_case_name_dict()
+        self.test_case_dict = self.get_case_name_dict()
+        # test_case_dict = {
+        #     case_path1:{
+        #         case_postfix1:[case_name1,...]
+        #         case_postfix2:[case_name1,...]}
+        # },
+        # ...
 
     def get_case_name_dict(self):
-        file_dict = dict()
+        test_case_dict = dict()
         for case_path in self.case_path_list:
+            test_case_dict[case_path] = dict()
             for postfix in self.postfix_list:
                 pattern = re.compile(r'(.*)(' + postfix + ')$')
-                file_dict[postfix] = list()
-                for file_name in os.listdir(case_path):
-                    if file_name in self.ignore_case:
+                test_case_dict[case_path][postfix] = list()
+                for case_name in os.listdir(case_path):
+                    if case_name in self.ignore_case:
                         continue
-                    if pattern.match(file_name):
-                        file_dict[postfix].append(file_name)
-        return file_dict
+                    if pattern.match(case_name):
+                        test_case_dict[case_path][postfix].append(case_name)
+        return test_case_dict
 
     def analysis(self):
-        for file_postfix in self.file_dict:
-            if len(self.file_dict[file_postfix]) == 0:
-                continue
-            module_path = self.type_analyst[file_postfix].split('.')
-            module_name = '.'.join(module_path[:-2])
-            analyst_module = import_module(module_name)
-            analyst_object = getattr(analyst_module, module_path[-2])
-            a = analyst_object()
-            analyst_method = getattr(a, module_path[-1])
-            analyst_method()
+        for case_path in self.test_case_dict:
+            for case_postfix in self.test_case_dict[case_path]:
+                if len(self.test_case_dict[case_path][case_postfix]) == 0:
+                    continue
+                module_path = self.type_analyst[case_postfix].split('.')
+                module_name = '.'.join(module_path[:-2])
+                analyst_module = import_module(module_name)
+                analyst_object = getattr(analyst_module, module_path[-2])
+                analyst_method = getattr(analyst_object(), module_path[-1])
+                for case_name in self.test_case_dict[case_path][case_postfix]:
+                    analysis_result = analyst_method(case_name)
+
 
 
 if __name__ == '__main__':
