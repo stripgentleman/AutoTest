742e5cc9eb21a3792f843ddd74f67ab2fe01ab63
huangwenda <417198820@qq.com>
2021-02-19 18:19:30 +0800

    区分action和assertion的执行methodDict

--- src/handlersLoader.py
+++ src/handlersLoader.py
@@ -5,7 +5,11 @@ from src.tools.handlersLoad.actionHandlersLoaders.actionHandlersLoader import Ac
 
 class HandlersLoader:
 
-    load_method = dict()
+    ACTION = 'action'
+    ASSERTION = 'assertion'
+
+    load_action_method = dict()
+    load_assertion_method = dict()
 
     @staticmethod
     def method_call(fun, arg_dict: dict):
@@ -24,19 +28,23 @@ class HandlersLoader:
         fun(*args)
 
     @staticmethod
-    def tag_call_method(tag, args_dict: dict):
-        if HandlersLoader.load_method.get(tag) is None:
-            HandlersLoader.load_method[tag] = ActionHandlersLoader.load(tag)
-        HandlersLoader.method_call(HandlersLoader.load_method[tag], args_dict)
-
-
-def tes(aaa, bbb):
-    print(aaa, bbb)
+    def tag_call_method(action_assertion, tag, args_dict: dict):
+        if action_assertion == HandlersLoader.ACTION:
+            load_method = HandlersLoader.load_action_method
+        elif action_assertion == HandlersLoader.ASSERTION:
+            load_method = HandlersLoader.load_assertion_method
+        else:
+            error_msg = ''.join(['HandlersLoader.tag_call_method() get error action_assertion value : ',
+                                 action_assertion, ' , it only required \'', HandlersLoader.ACTION, '\' or \'', HandlersLoader.ASSERTION, '\''])
+            raise TypeError(error_msg)
+        if load_method.get(tag) is None:
+            load_method[tag] = ActionHandlersLoader.load(tag)
+        HandlersLoader.method_call(load_method[tag], args_dict)
 
 
 if __name__ == '__main__':
     # HandlersLoader.method_call(tes, {'aaa': 222, 'bbb':321321})
-    HandlersLoader.tag_call_method('test', {'param1':111, 'param2':111, 'param3':111})
-    HandlersLoader.tag_call_method('test', {'param1': 151, 'param2': 1771, 'param3': 121})
-    HandlersLoader.tag_call_method('test', {'param1': 311, 'param2': 511, 'param3': 131})
-    HandlersLoader.tag_call_method('test', {'param3': 111, 'param1': 151, 'param2': 141})
+    HandlersLoader.tag_call_method('action', 'test', {'param1':111, 'param2':111, 'param3':111})
+    HandlersLoader.tag_call_method('action', 'test', {'param1': 151, 'param2': 1771, 'param3': 121})
+    HandlersLoader.tag_call_method('action', 'test', {'param1': 311, 'param2': 511, 'param3': 131})
+    HandlersLoader.tag_call_method('action', 'test', {'param3': 111, 'param1': 151, 'param2': 141})
