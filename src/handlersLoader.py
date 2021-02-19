53f61e9af01ecff5e14cb3bd42d6a8d7193dba6a
huangwenda <417198820@qq.com>
2021-02-19 10:21:52 +0800

    完善函数参数缺失的判断

--- src/handlersLoader.py
+++ src/handlersLoader.py
@@ -1,9 +1,10 @@
 import inspect
 
+
 class HandlersLoader:
 
     @staticmethod
-    def method_call(fun, arg_dict:dict):
+    def method_call(fun, arg_dict: dict):
         fun_signature = inspect.signature(fun)
         arg_list = list(fun_signature.parameters)
 
@@ -12,13 +13,15 @@ class HandlersLoader:
             arg_value = arg_dict.get(arg)
             if arg_value is None:
                 arg_value = fun_signature.parameters[arg].default
+                if arg_value == inspect.Parameter.empty:
+                    raise TypeError(fun.__name__ + '() missing 1 required positional argument: ' + arg)
             args.append(arg_value)
         fun(*args)
 
 
-def tes(aaa, bbb='=='):
-    print(aaa,bbb)
+def tes(aaa, bbb):
+    print(aaa, bbb)
 
 
 if __name__ == '__main__':
-    HandlersLoader.method_call(tes,{'aaa':222})
+    HandlersLoader.method_call(tes, {'aaa': 222, 'bbb':321321})
