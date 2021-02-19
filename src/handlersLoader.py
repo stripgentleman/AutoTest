8b23920f9450f3ef0bdf1e839e066aba29ebd8bd
huangwenda <417198820@qq.com>
2021-02-19 17:57:19 +0800

    完成初版handler调用逻辑

--- src/handlersLoader.py
+++ src/handlersLoader.py
@@ -1,7 +1,11 @@
 import inspect
 
+from src.tools.handlersLoad.actionHandlersLoaders.actionHandlersLoader import ActionHandlersLoader
+
 
 class HandlersLoader:
+
+    load_method = dict()
 
     @staticmethod
     def method_call(fun, arg_dict: dict):
@@ -14,9 +18,16 @@ class HandlersLoader:
             if arg_value is None:
                 arg_value = fun_signature.parameters[arg].default
                 if arg_value == inspect.Parameter.empty:
-                    raise TypeError(fun.__name__ + '() missing 1 required positional argument: ' + arg)
+                    error_msg = ''.join([fun.__module__, '.', fun.__name__, '() ', 'get arguments : ', str(arg_dict), ', but missing required positional argument: ', arg])
+                    raise TypeError(error_msg)
             args.append(arg_value)
         fun(*args)
+
+    @staticmethod
+    def tag_call_method(tag, args_dict: dict):
+        if HandlersLoader.load_method.get(tag) is None:
+            HandlersLoader.load_method[tag] = ActionHandlersLoader.load(tag)
+        HandlersLoader.method_call(HandlersLoader.load_method[tag], args_dict)
 
 
 def tes(aaa, bbb):
@@ -24,4 +35,8 @@ def tes(aaa, bbb):
 
 
 if __name__ == '__main__':
-    HandlersLoader.method_call(tes, {'aaa': 222, 'bbb':321321})
+    # HandlersLoader.method_call(tes, {'aaa': 222, 'bbb':321321})
+    HandlersLoader.tag_call_method('test', {'param1':111, 'param2':111, 'param3':111})
+    HandlersLoader.tag_call_method('test', {'param1': 151, 'param2': 1771, 'param3': 121})
+    HandlersLoader.tag_call_method('test', {'param1': 311, 'param2': 511, 'param3': 131})
+    HandlersLoader.tag_call_method('test', {'param3': 111, 'param1': 151, 'param2': 141})
--- src/tools/handlersLoad/actionHandlersLoaders/actionHandlersLoader.py
+++ src/tools/handlersLoad/actionHandlersLoaders/actionHandlersLoader.py
@@ -1,5 +1,4 @@
 from importlib import import_module
-import inspect
 
 import src.config.actionHandleConfig as Config
 
@@ -8,26 +7,34 @@ class ActionHandlersLoader:
     ac_methods = Config.ac_methods
 
     @staticmethod
-    def load():
+    def load(tag=None):
+        print('load')
         methods_dict = dict()
         ac_methods = ActionHandlersLoader.ac_methods
-        for tag in ac_methods:
-            if ac_methods.get(tag) is not None:
-                method_path = ac_methods[tag].split('.')
-                method_module = import_module('.'.join(method_path[:-2]))
-                method_object = getattr(method_module, method_path[-2])
-                method = getattr(method_object, method_path[-1])
-                methods_dict[tag] = method
-        return methods_dict
+        if tag is None:
+            for temp_tag in ac_methods:
+                if ac_methods.get(temp_tag) is not None:
+                    methods_dict[temp_tag] = ActionHandlersLoader.load_method(ac_methods[temp_tag])
+            return methods_dict
+        else:
+            return ActionHandlersLoader.load_method(ac_methods.get(tag))
+
+    @staticmethod
+    def load_method(method_path: str):
+        path_list = method_path.split('.')
+        method_module = import_module('.'.join(path_list[:-2]))
+        method_object = getattr(method_module, path_list[-2])
+        method = getattr(method_object, path_list[-1])
+        return method
 
 
 if __name__ == '__main__':
     aa = ActionHandlersLoader.load()
 
     print(aa)
-    aa['test'](32,13,1)
-
-    ap = inspect.signature(aa['test'])
-    print(list(ap.parameters))
+    # aa['test'](32,13,1)
+    #
+    # ap = inspect.signature(aa['test'])
+    # print(list(ap.parameters))
 
 
