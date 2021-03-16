34ce43f68decba0d298cbaa69c2855abe9f5f2f5
huangwenda <417198820@qq.com>
2021-03-16 17:29:00 +0800

    初步实现manager，后续实现opt控制

---
+++ manager.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import getopt
+
+path = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep))
+if path not in sys.path:
+    sys.path.append(path)
+
+from src.driver import Driver
+
+driver = Driver()
+driver.run_all_handler()
