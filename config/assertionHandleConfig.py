39ecc9b643288a5dc7b4709d6d42092a5d9f339a
huangwenda <417198820@qq.com>
2021-03-16 17:18:47 +0800

    补充config

---
+++ config/assertionHandleConfig.py
@@ -0,0 +1,11 @@
+#############################################
+# 断言handle配置信息
+#############################################
+
+default_package_path = 'handlers.assertionHandlers.'
+assertion_key = 'AS'
+
+as_methods = {
+    # 标志:方法路径
+    'test': default_package_path + 'testHandler.TestHandler.testmethod'
+}
---
+++ config/config.py
@@ -0,0 +1,13 @@
+import os
+import logging
+
+base_path = os.path.sep.join(os.path.abspath(__file__).split(os.path.sep)[:-2])
+
+##############################
+# log相关配置
+##############################
+running_log_path = base_path + os.path.sep + 'testCase' + os.path.sep + 'testCaseRunningLog.log'
+log_enable = True
+log_level = logging.DEBUG
+log_format = '%(levelname)s - %(asctime)s]: %(message)s'
+only_save_last = True
